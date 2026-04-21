f25e302e45f403b2dab4a306b3791ea004c573db
Myatthu2883 <myatthu2883@gmail.com>
2026-04-22 04:38:20 +0700

    One change

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME,
-  port:     Number(process.env.DB_PORT || 38691),
+  port:     Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: 3,       // keep low for serverless
   queueLimit: 0,
